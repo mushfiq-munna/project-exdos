701ef0a718f8a6391214edd7edd9b855b4f7fd91
Mushfiq Munna <munna.r.mushfiqur@gmail.com>
2025-07-17 04:25:10 +0600

    service and testimonial section added

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+(function ($) {
+  "use strict";
+
+  // testimonial slider
+  var swiper = new Swiper(".tp-testimonial-active", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    keyboard: {
+      enabled: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".tp-swiper-test-button-next",
+      prevEl: ".tp-swiper-test-button-prev",
+    },
+  });
+})(jQuery);
