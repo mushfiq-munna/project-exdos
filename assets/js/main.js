4e5fd799ac1d44b7eaf397de7873459c7e696144
Mushfiq Munna <munna.r.mushfiqur@gmail.com>
2025-07-18 05:21:03 +0600

    brand and team section added

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,4 +17,50 @@
       prevEl: ".tp-swiper-test-button-prev",
     },
   });
+
+  // brand slider
+  var swiper = new Swiper(".tp-brand-top-active", {
+    slidesPerView: "auto",
+    spaceBetween: 60,
+    freemode: true,
+    centeredSlides: true,
+    loop: true,
+    speed: 4000,
+    allowTouchMove: false,
+    autoplay: {
+      delay: 1,
+      disableOnInteraction: false,
+    },
+  });
+
+  var swiper = new Swiper(".tp-brand-bottom-active", {
+    slidesPerView: "auto",
+    spaceBetween: 60,
+    freemode: true,
+    centeredSlides: true,
+    loop: true,
+    speed: 4000,
+    allowTouchMove: false,
+    autoplay: {
+      delay: 1,
+      disableOnInteraction: false,
+    },
+  });
+
+  // team slider
+  var swiper = new Swiper(".tp-team-active", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    keyboard: {
+      enabled: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".tp-swiper-team-button-next",
+      prevEl: ".tp-swiper-team-button-prev",
+    },
+  });
 })(jQuery);
