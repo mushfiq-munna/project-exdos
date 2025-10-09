a06b190e78db41e71cd39603b08613032ceedb99
Mushfiq Munna <munna.r.mushfiqur@gmail.com>
2025-10-10 05:20:48 +0600

    home-2 responsive done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,45 @@
 (function ($) {
   "use strict";
 
+  // mobile menu
+  var tpMenuWrap = $(".tp-mobile-menu-active > ul").clone();
+  var tpSideMenu = $(".tp-offcanvas-menu nav");
+  tpSideMenu.append(tpMenuWrap);
+  if ($(tpSideMenu).find(".sub-menu, .tp-mega-menu").length != 0) {
+    $(tpSideMenu)
+      .find(".sub-menu, .tp-mega-menu")
+      .parent()
+      .append(
+        '<button class="tp-menu-close"><i class="fas fa-chevron-right"></i></button>'
+      );
+  }
+
+  var sideMenuList = $(
+    ".tp-offcanvas-menu nav > ul > li button.tp-menu-close, .tp-offcanvas-menu nav > ul li.has-dropdown > a"
+  );
+  $(sideMenuList).on("click", function (e) {
+    console.log(e);
+    e.preventDefault();
+    if (!$(this).parent().hasClass("active")) {
+      $(this).parent().addClass("active");
+      $(this).siblings(".sub-menu, .tp-mega-menu").slideDown();
+    } else {
+      $(this).siblings(".sub-menu, .tp-mega-menu").slideUp();
+      $(this).parent().removeClass("active");
+    }
+  });
+
+  // class add
+  $(".tp-offcanvas-toggle").on("click", function () {
+    $(".tp-offcanvas").addClass("tp-offcanvas-open");
+    $(".tp-offcanvas-overlay").addClass("tp-offcanvas-overlay-open");
+  });
+
+  $(".tp-offcanvas-close, .tp-offcanvas-overlay").on("click", function () {
+    $(".tp-offcanvas").removeClass("tp-offcanvas-open");
+    $(".tp-offcanvas-overlay").removeClass("tp-offcanvas-overlay-open");
+  });
+
   // data bg img
   $("[data-background]").each(function () {
     $(this).css(
@@ -45,6 +84,24 @@
       prevEl: ".tp-swiper-test-button-prev",
     },
   });
+
+  // // testimonial slider
+  // var swiper = new Swiper(".tp-testimonial-active-2", {
+  //   slidesPerView: 1,
+  //   effect: "fade",
+  //   spaceBetween: 30,
+  //   keyboard: {
+  //     enabled: true,
+  //   },
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true,
+  //   },
+  //   navigation: {
+  //     nextEl: ".tp-swiper-test-button-next",
+  //     prevEl: ".tp-swiper-test-button-prev",
+  //   },
+  // });
 
   // brand slider
   var swiper = new Swiper(".tp-brand-top-active", {
